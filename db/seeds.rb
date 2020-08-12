19d4b7ddee6a909821e0efbf0c69d57e151c6fe5
Emma Colombat <emma.colombat69@gmail.com>
2020-08-12 13:52:13 +0200

    created fake data for Restaurant and Review in seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
 
-# puts "Creating 50 restaurants..."
-# 50.times do
-#   restaurant = Restaurant.new(
-#     name:         Faker::Restaurant.name,
-#     address:      Faker::Address.street_address,
-#     phone_number: Faker::PhoneNumber.cell_phone,
-#     category:     ["chinese", "italian", "japanese", "french", "belgian"].sample
-#   )
-#   restaurant.save!
-# end
+puts "Creating 50 restaurants..."
+50.times do
+  restaurant = Restaurant.new(
+    name:         Faker::Restaurant.name,
+    address:      Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category:     ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+end
 puts "Creating 50 Reviews..."
 50.times do
   review = Review.new(
     content: Faker::Lorem.sentences,
     rating:  rand(0..5),
   )
+  review.restaurant = Restaurant.all.sample
   review.save!
 end
 puts "Finished!"
